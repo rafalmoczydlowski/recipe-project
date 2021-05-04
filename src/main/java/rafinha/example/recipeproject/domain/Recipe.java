bf8be0a468765f23042621b468789405aaa16274
rafalmoczydlowski <moczydlowski7@wp.pl>
2021-05-04 20:49:46 +0200

    Add Ingredient class. OneToMany and ManyToOne Relationships

--- src/main/java/rafinha/example/recipeproject/domain/Recipe.java
+++ src/main/java/rafinha/example/recipeproject/domain/Recipe.java
@@ -1,6 +1,7 @@
 package rafinha.example.recipeproject.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -22,6 +23,9 @@ public class Recipe {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     public Long getId() {
         return id;
@@ -101,5 +105,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
