1870d720970095a342efcb358d34cd82a4fcb92e
rafalmoczydlowski <moczydlowski7@wp.pl>
2021-05-18 14:38:20 +0200

    ArgumentCaptor - przechwytywanie argumentów

--- src/test/java/rafinha/example/recipeproject/controllers/IndexControllerTest.java
+++ src/test/java/rafinha/example/recipeproject/controllers/IndexControllerTest.java
@@ -3,16 +3,19 @@ package rafinha.example.recipeproject.controllers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+import rafinha.example.recipeproject.domain.Recipe;
 import rafinha.example.recipeproject.services.RecipeService;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class IndexControllerTest {
 
@@ -24,24 +27,48 @@ class IndexControllerTest {
     @Mock
     private RecipeService recipeService;
 
+    Set<Recipe> recipeSet = new HashSet<>();
+
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
         indexController = new IndexController(recipeService);
     }
 
+
     @AfterEach
     void tearDown() throws Exception {
         MockitoAnnotations.openMocks(this).close();
     }
 
+    private Set<Recipe> setUpRecipes() {
+        Recipe recipe1 = new Recipe();
+        recipe1.setId(1L);
+        Recipe recipe2 = new Recipe();
+        recipe2.setId(2L);
+
+        recipeSet.add(recipe1);
+        recipeSet.add(recipe2);
+
+        return recipeSet;
+    }
+
     @Test
     void getIndexPage() {
+        recipeSet = setUpRecipes();
+
+        when(recipeService.getRecipies()).thenReturn(recipeSet);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
         String viewName = indexController.getIndexPage(model);
 
         assertEquals("index", viewName);
 
         verify(recipeService, times(1)).getRecipies();
-        verify(model, times(1)).addAttribute(eq("recipies"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipies"), argumentCaptor.capture());
+
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 }
