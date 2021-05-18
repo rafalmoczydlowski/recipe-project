e526d69913446543dd25ceb160a39dda01265650
rafalmoczydlowski <moczydlowski7@wp.pl>
2021-05-18 14:56:56 +0200

    Spring MockMvc Introduction

--- src/test/java/rafinha/example/recipeproject/controllers/IndexControllerTest.java
+++ src/test/java/rafinha/example/recipeproject/controllers/IndexControllerTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 import rafinha.example.recipeproject.domain.Recipe;
 import rafinha.example.recipeproject.services.RecipeService;
@@ -16,6 +18,9 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 class IndexControllerTest {
 
@@ -35,22 +40,18 @@ class IndexControllerTest {
         indexController = new IndexController(recipeService);
     }
 
-
     @AfterEach
     void tearDown() throws Exception {
         MockitoAnnotations.openMocks(this).close();
     }
 
-    private Set<Recipe> setUpRecipes() {
-        Recipe recipe1 = new Recipe();
-        recipe1.setId(1L);
-        Recipe recipe2 = new Recipe();
-        recipe2.setId(2L);
+    @Test
+    void getIndexPageMockMvc() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
 
-        recipeSet.add(recipe1);
-        recipeSet.add(recipe2);
-
-        return recipeSet;
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
     }
 
     @Test
@@ -70,5 +71,17 @@ class IndexControllerTest {
 
         Set<Recipe> setInController = argumentCaptor.getValue();
         assertEquals(2, setInController.size());
+    }
+
+    private Set<Recipe> setUpRecipes() {
+        Recipe recipe1 = new Recipe();
+        recipe1.setId(1L);
+        Recipe recipe2 = new Recipe();
+        recipe2.setId(2L);
+
+        recipeSet.add(recipe1);
+        recipeSet.add(recipe2);
+
+        return recipeSet;
     }
 }
