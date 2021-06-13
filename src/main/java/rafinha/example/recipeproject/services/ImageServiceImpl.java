0ffea21c932ec0a3d61c2e5e9929b79e0c0f29c9
rafalmoczydlowski <moczydlowski7@wp.pl>
2021-06-13 13:52:04 +0200

    Persisitng images to Database

--- src/main/java/rafinha/example/recipeproject/services/ImageServiceImpl.java
+++ src/main/java/rafinha/example/recipeproject/services/ImageServiceImpl.java
@@ -3,13 +3,42 @@ package rafinha.example.recipeproject.services;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import rafinha.example.recipeproject.domain.Recipe;
+import rafinha.example.recipeproject.repositories.RecipeRepository;
+
+import java.io.IOException;
 
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
 
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
     public void saveImageFile(Long id, MultipartFile file) {
-        log.debug("Recived a File");
+        try {
+            // get the Recipe obj out of the repo [going to saving against an exisitng recipe]
+            Recipe recipe = recipeRepository.findById(id).get();
+            // convert to wrapper class of a Byte into Byte object
+            Byte[] byteObj = new Byte[file.getBytes().length];
+
+            // set up iterator to iterate over that and
+            // convert over the Byte Object from Java primitive to the wrapper object
+            int i = 0;
+
+            for (byte b : file.getBytes())
+                byteObj[i++] = b;
+
+            recipe.setImage(byteObj);
+            recipeRepository.save(recipe);
+        }
+        catch (IOException ex) {
+            log.error("Error occurred", ex);
+            ex.printStackTrace();
+        }
     }
 }
--- src/test/java/rafinha/example/recipeproject/services/ImageServiceImplTest.java
+++ src/test/java/rafinha/example/recipeproject/services/ImageServiceImplTest.java
@@ -3,18 +3,31 @@ package rafinha.example.recipeproject.services;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+import rafinha.example.recipeproject.domain.Recipe;
+import rafinha.example.recipeproject.repositories.RecipeRepository;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 
 class ImageServiceImplTest {
+
+    @Mock
+    RecipeRepository recipeRepository;
 
     ImageService imageService;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
-        imageService = new ImageServiceImpl();
+        imageService = new ImageServiceImpl(recipeRepository);
     }
 
     @AfterEach
@@ -23,8 +36,30 @@ class ImageServiceImplTest {
     }
 
     @Test
-    void saveImageFile() {
-        String expected = "TEST";
-        assertEquals("TEST", expected);
+    void saveImageFile() throws Exception {
+        // given
+        Long id = 1L;
+        MultipartFile multipartFile = new MockMultipartFile("imagefile", "testing.txt",
+                "text/plain", "Rafinha Example".getBytes());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(id);
+        Optional<Recipe> optionalRecipe = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(optionalRecipe);
+
+        ArgumentCaptor<Recipe> recipeArgumentCaptor = ArgumentCaptor.forClass(Recipe.class);
+
+        // when
+        imageService.saveImageFile(id, multipartFile);
+
+        // then
+        verify(recipeRepository, times(1)).findById(anyLong());
+        // verifying that the RecipeRepository's save method is called one time and
+        verify(recipeRepository, times(1)).save(recipeArgumentCaptor.capture());
+        // capture argument going to give a savedRecipe
+        Recipe savedRecipe = recipeArgumentCaptor.getValue();
+        // check to see if the bytes match
+        assertEquals(multipartFile.getBytes().length, savedRecipe.getImage().length);
     }
 }
